d520dd4458a9721f383c458d5343998e683f9a78
alaouirania <75624037+alaouirania@users.noreply.github.com>
2022-01-21 16:38:34 +0100

    capital indexes

---
+++ capital_indexes.py
@@ -0,0 +1,24 @@
+# Write a function named capital_indexes.
+# The function takes a single parameter, which is a string.
+# Your function should return a list of all the indexes in the string that have capital letters.
+# For example, calling capital_indexes("HeLlO") should return the list [0, 2, 4]
+
+def capital_indexes(s):
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = []
+    for i, l in enumerate(s):
+        if l in upper:
+            result.append(i)
+    return result
+
+
+print(capital_indexes("TesTT"))
+
+
+# or
+
+def capital_indexes(string):
+    return [i for i, j in enumerate(string) if j.isupper()]
+
+
+string = "HeyfsdfFSDFSDF"
